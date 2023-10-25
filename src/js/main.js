4de754ee9420f76e35f3acbd218a840fab569911
Nikita Titov <netitov@gmail.com>
2023-10-25 10:00:32 +0200

    player layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-console.log('test')
+import '../assets/styles/index.css';
+
+let slider = document.querySelector('.player__seek-slider-value');
+
+function calcValue() {
+  let valuePercentage = (slider.value / slider.max) * 100;
+  slider.style.background = `linear-gradient(to right, #b8630f ${valuePercentage}%, #77777754 ${valuePercentage}%)`;
+}
+
+// Update the current slider value (each time you drag the slider handle)
+slider.addEventListener('input', function(){
+  calcValue();
+})
+
+calcValue();
